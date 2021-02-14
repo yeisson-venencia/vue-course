48f96fc6ceae5dbee149c53d9555ee6effb698ee
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-14 16:34:36 -0400

    s11/class-one: adding style to rating control component

--- vue-forms/src/components/RatingControl.vue
+++ vue-forms/src/components/RatingControl.vue
@@ -5,3 +5,21 @@
     <li><button>Great</button></li>
   </ul>
 </template>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
