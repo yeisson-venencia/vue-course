2a478b58ac7cb972a588d8a687f468b67894bfe3
yeisson-venencia <yvenencia.saurith@gmail.com>
2021-02-14 02:08:56 -0400

    s11/class-one: v-modeling with radio buttons and checkbox

--- vue-forms/src/components/TheForm.vue
+++ vue-forms/src/components/TheForm.vue
@@ -24,32 +24,77 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input
+          id="interest-news"
+          name="interest"
+          type="checkbox"
+          value="news"
+          v-model="interest"
+        />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input
+          id="interest-tutorials"
+          name="interest"
+          type="checkbox"
+          value="tutorials"
+          v-model="interest"
+        />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input
+          id="interest-nothing"
+          name="interest"
+          type="checkbox"
+          value="nothing"
+          v-model="interest"
+        />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input
+          id="how-video"
+          name="how"
+          type="radio"
+          value="video"
+          v-model="how"
+        />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input
+          id="how-blogs"
+          name="how"
+          type="radio"
+          value="blogs"
+          v-model="how"
+        />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input
+          id="how-other"
+          name="how"
+          type="radio"
+          value="others"
+          v-model="how"
+        />
         <label for="how-other">Other</label>
       </div>
+    </div>
+    <div id="form-control">
+      <input
+        type="checkbox"
+        name="confirm-terms"
+        id="confirm-terms"
+        v-model="confirm"
+      />
+      <label for="confirm-terms">Agree to terms of use?</label>
     </div>
     <div>
       <button type="submit">Save Data</button>
@@ -63,18 +108,32 @@ export default {
     return {
       userName: '',
       userAge: null,
-      referrer: 'wom'
+      referrer: 'wom',
+      interest: [],
+      how: null,
+      confirm: false
     };
   },
   methods: {
     submitForm() {
-      console.log('this.userName :>> ', this.userName);
+      const formData = {
+        name: this.userName,
+        age: this.userAge,
+        referrer: this.referrer,
+        interest: this.interest,
+        how: this.how,
+        confirm: this.confirm
+      };
+      console.log(formData);
       this.resetForm();
     },
     resetForm() {
       this.userName = '';
       this.userAge = null;
       this.referrer = 'wom';
+      this.interest = [];
+      this.how = null;
+      this.confirm = false;
     }
   }
 };
